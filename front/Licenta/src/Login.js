476f137d6b2a9caf894e3fc91ea55947ac8e2233
Apostol Alexandru <alexapostol0118@gmail.com>
2022-11-06 22:15:08 +0200

    PDF generated

--- front/Licenta/src/Login.js
+++ front/Licenta/src/Login.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { Navigate } from "react-router-dom";
 import { useAlert } from "react-alert";
 import validator from 'validator'
-
+import jsPDF from 'jspdf'
 
 
 
@@ -59,11 +59,157 @@ const Plata = ({setToken, token, setName, setRole}) => {
   const [motiv, setMotiv] = useState("");
 
 
+  function generatepdf(){
+    const doc = new jsPDF("p", "pt", "a4"); // default values
 
+    // set font
+    doc.setFont("helvetica", "bold");
+
+    // font size
+    doc.setFontSize(35);
+
+    // title, centered around x
+    // doc.text(text, x, y, flags, angle, align);
+    var top = 20;
+    doc.text(
+      "Dovada de plata amenda rutiera",
+      105 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "center"
+    );
+    top+=20;
+      // set font
+    doc.setFont("helvetica");
+
+    // font size
+    doc.setFontSize(16);
+    doc.text(
+      "Prin prezenta se dovedeste faptul ca subsemnatul:",
+      40 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    top += 10;
+    var np = nume+" "+prenume + " identificat prin CNP " + cnp + " domiciliat in ";
+    doc.text(
+      np,
+      1 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    top+=10;
+    var dom = adresa + " Judetul " + judet;
+    doc.text(
+      dom,
+      1 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    top+=20
+    var val = "In cuantum de " + suma + " lei"
+    doc.text(
+      val,
+      105 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "center"
+    );
+    top +=10 
+    var mot = "In baza motivului: " + motiv;
+    doc.text(
+      mot,
+      105 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "center"
+    );
+    top+=10
+    var data = "Fapta savarsita la data de " + date
+    doc.text(
+      data,
+      105 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "center"
+    );
+    top+=50
+    if(reducere(data) == true){
+      var achitat = "Suma totala de achitat: " + suma/2 + "lei";
+    doc.text(
+      achitat,
+      105 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "center"
+    );
+    top+=5;
+    doc.setFontSize(7);
+    doc.text(
+      "**Valoarea de plata este jumatate din valuarea totala a amenzii deoarece aceasta a fost achitata in mai putin de 15 zile de la data emiterii",
+      1 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    }else{
+      var achitat = "Suma totala de achitat: " + suma + "lei";
+    doc.text(
+      achitat,
+      105 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "center"
+    );
+    }
+    doc.setFontSize(16);
+    top = 260
+    doc.text(
+      "Data",
+      45 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    doc.text(
+      "Semnatura",
+      135 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    top+=10
+    var dataactuala = new Date()
+    var azi = dataactuala.getFullYear() + '-' + (dataactuala.getMonth() + 1) + '-' + dataactuala.getDate();
+    doc.text(
+      azi,
+      40 * 2.83,
+      top * 2.83,
+      null,
+      null,
+      "left"
+    );
+    doc.save("Dovada de plata.pdf");
+  }
  
   
   const handleSubmit = async (e) => {
-    
+    generatepdf();
   }
 
   
@@ -161,8 +307,8 @@ const Plata = ({setToken, token, setName, setRole}) => {
                           suma === ''?(
                             alert.error("Va rugam introduceti suma amenzii")
                           ):(
-                            alert.success("Plata efectuata cu succes")
-                            //handleSubmit()
+                            //alert.success("Plata efectuata cu succes")
+                            handleSubmit()
                           )
                         )
                       )
