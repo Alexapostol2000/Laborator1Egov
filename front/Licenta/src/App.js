fec74868e9cb3bb09508cc9e0b9a19ce30334254
Apostol Alexandru <alexapostol0118@gmail.com>
2022-11-08 00:57:16 +0200

    cleanup code

--- front/Licenta/src/App.js
+++ front/Licenta/src/App.js
@@ -1,19 +1,16 @@
-import logo from './logo.svg';
+
 import * as React from 'react';
 import './App.scss';
 import Plata from './Plata';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar';
 import Home from './Home';
-import Register from './Register';
-import Account from './Account';
+
 import useToken from './hooks/useToken';
 import useRole from './hooks/useRole';
 import useName from './hooks/useName';
-import Logout from './Logout';
-import Password from './password';
-import Upload from './Upload';
-import useAdminToken from './hooks/useAdminToken';
+
+
 
 
 
@@ -31,12 +28,6 @@ function App() {
       <Routes>
         <Route exact path="/" element = {<Home />}/>
         <Route path="/plata" element = {<Plata setToken={setToken} token={token}  setName={setName} setRole={setRole}/>}/>
-        <Route path="/register" element = {<Register setRole={setRole} role={role} token={token}  />}/>
-        <Route path="/account" element = {<Account  token={token}   name={name} role={role}/>}/>
-        <Route path="/logout" element = {<Logout setToken={setToken} token={token} setRole={setRole} role={role}/>}/>
-        <Route path="/password" element = {<Password role={role} token={token}/>}/>
-        <Route path="/upload" element = {<Upload  name={name} role={role} token={token}  />}/>
-
       </Routes>
       </div>
     </div>
